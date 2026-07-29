a7f08578e9c710ff3b5c9d9ccf15b5a0381e9d23
VaibhavDW789 <vaibhavw97531@gmail.com>
2026-07-29 11:09:25 +0000

    Create reusable VPC module

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -3,3 +3,21 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "AWS-3Tier-Infrastructure"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
